0f0dab3f310d2bd45bd28495693f6a11381b7c71
Dima121512 <dimaternov1215@gmail.com>
2023-10-22 23:40:30 +0200

    Commit css,js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ $(function(){
 
 
 
-    $('.bike__slider').slick();
+    $('.bike__slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 })
