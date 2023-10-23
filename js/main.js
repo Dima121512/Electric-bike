802c2ce4197d446177b4614ac5396317fadcf999
Dima121512 <dimaternov1215@gmail.com>
2023-10-23 14:14:22 +0200

    Commit end code

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+    $(".menu, .bike__column").on("click","a", function (event) {
+		event.preventDefault();
+
+		var id  = $(this).attr('href'),
+
+			top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 })
